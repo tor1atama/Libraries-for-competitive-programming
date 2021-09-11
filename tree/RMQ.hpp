888e21646b49a6e9126d93d73e4378974969a698
tor1atama <87610496+tor1atama@users.noreply.github.com>
2021-09-11 11:37:38 +0900

    Update RMQ.hpp

--- tree/RMQ.hpp
+++ tree/RMQ.hpp
@@ -1,31 +1,30 @@
-//using ll=long long;
 struct RMQ{
-    const ll e=1LL<<40;
+    const long long e=1LL<<60;
     int n;
-    vector<ll> a;
+    vector<long long> a;
     RMQ(int n_){
         n=1;
         while(n<n_*2) n*=2;
         a.resize(n*2,e);
     }
-    void set(int i,ll x){
+    void set(int i,long long x){
         i+=n;
         while(i){
-            chmin(a[i],x);
+            a[i]=min(a[i],x);
             i/=2;
         }
     }
-	ll get(int i){
+	long long get(int i){
 		return a[n+i];
 	}
-    ll prod(int l,int r){
+    long long prod(int l,int r){
         l+=n;
         r+=n;
-        ll res=e;
+        long long res=e;
         while(l<r){
-            if(l&1) chmin(res,a[l++]);
+            if(l&1) res=min(res,a[l++]);
             l/=2;
-            if(r&1) chmin(res,a[--r]);
+            if(r&1) res=min(res,a[--r]);
             r/=2;
         }
         return res;
