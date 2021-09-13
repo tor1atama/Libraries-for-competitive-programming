58dcc681b06742948325676a2d1a7e3d7300b693
tor1atama <87610496+tor1atama@users.noreply.github.com>
2021-09-13 09:03:07 +0900

    Create UnionFind.hpp

---
+++ data structure/UnionFind.hpp
@@ -0,0 +1,28 @@
+struct UnionFind {
+    vector<int> par;
+    int num; //the number of connected components
+    UnionFind(int n){
+        par.assign(n,-1);
+        num=n;
+    }
+    int root(int x){
+        if(par[x]<0) return x;
+        return par[x]=root(par[x]);
+    }
+    bool same(int x,int y){
+        return root(x)==root(y);
+    }
+    int size(int x){
+        return -par[root(x)];
+    }
+    bool unite(int x,int y){
+        x=root(x);
+        y=root(y);
+        if(x==y) return 0;
+        if(par[x]>par[y]) swap(x,y);
+        par[x]+=par[y];
+        par[y]=x;
+        num--;
+        return 1;
+    }
+};
