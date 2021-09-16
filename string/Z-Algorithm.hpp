c0eec2c8a70ccd6b1eed311b3c7a175421497120
tor1atama <87610496+tor1atama@users.noreply.github.com>
2021-09-16 10:38:47 +0900

    Create Z-Algorithm.hpp

---
+++ string/Z-Algorithm.hpp
@@ -0,0 +1,23 @@
+vector<int> Z_Algorithm(string &s){
+    int n=(int)s.size();
+    vector<int> res(n);
+    res[0]=n; //sometimes this value should be 0 (e.g. z of target+"$"+searched)
+    int l=r=0;
+    for(int i=1;i<n;++i){
+        if(r<i){
+            l=r=i;
+            while(r<n && s[r]==s[r-l]) ++r;
+            res[i]=r-l;
+            --r;
+        }else{
+            if(r-i+1>res[i-l]) {
+                res[i]=res[i-l];
+            }else{
+                l=i;
+                while(r<n && s[r+1]==s[r+1-l]) ++r;
+                res[i]=r-l+1;
+            }
+        }
+    }
+    return res;
+}
