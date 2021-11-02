a608795e89e5648d63cd3a39e412239cb461cd06
tor1atama <87610496+tor1atama@users.noreply.github.com>
2021-11-02 22:36:23 +0900

    updated rational.hpp

--- math/rational.hpp
+++ math/rational.hpp
@@ -1,23 +1,16 @@
 struct rational{
     long long INF=2e18;
-    long long num;
-    long long deno;
-    template<class T>T gcd(T a,T b){
+    long long num,deno;
+    long long gcd(long long a,long long b){
         if(b==0) return abs(a);
         return gcd(b,a%b);
     }
-    template<class T>rational(T n){
-        num=n;
-        deno=1;
-    }
-    template<class T>rational(T num_,T deno_){
-        if(num_==0&&deno_==0){
-            cout<<"error"<<endl;
+    rational(long long num_=0,long long deno_=1): num(num_),deno(deno_){
+        if(num==0&&deno==0){
+            cout<<"error: 0/0"<<endl;
             return;
         }
-        num=num_;
-        deno=deno_;
-        T g=gcd(num_,deno_);
+        long long g=gcd(num,deno);
         num/=g;
         deno/=g;
         if(deno<0){
@@ -29,13 +22,13 @@ struct rational{
         if(deno==0){return INF*num;}
         return (double)num/deno;
     }
-    bool operator==(rational &other){
+    bool operator==(const rational &other){
         return num==other.num && deno==other.deno;
     }
-    bool operator!=(rational &other){
-        return num!=other.num || deno!=other.deno;
+    bool operator!=(const rational &other){
+        return !(*this==other);
     }
-    bool operator<(rational &other){
+    bool operator<(const rational &other){
         if(*this==other) return 0;
         if(deno==0){
             if(num==1) return 0;
@@ -45,32 +38,23 @@ struct rational{
             if(other.num==1) return 1;
             else return 0;
         }
-        if(other.num==0) return num<0;
-        rational r=*this/other;
-        if(r.num<=0) return other.num>0;
-        if(r.num>r.deno) return num<0;
-        else return num>0;
-    }
-    bool operator>(rational &other){
-        if(*this==other) return 0;
-        if(deno==0){
-            if(num==1) return 1;
-            else return 0;
+        if (deno*other.deno>0){
+            return num*other.deno<deno*other.num;
+        }else{
+            return num*other.deno>deno*other.num;
         }
-        if(other.deno==0){
-            if(other.num==1) return 0;
-            else return 1;
-        }
-        if(other.num==0) return num>0;
-        rational r=*this/other;
-        if(r.num<=0) return other.num<0;
-        if(r.num>r.deno) return num>0;
-        else return num<0;
     }
-    bool operator<=(rational &other){return !(*this>other);}
-    bool operator>=(rational &other){return !(*this<other);}
-    rational operator+(rational &other){
-        if(deno==0) return rational(num,deno);
+    bool operator>(const rational &other){
+        return !(*this<=other);
+    }
+    bool operator<=(const rational &other){
+        return *this==other || *this<other;
+    }
+    bool operator>=(const rational &other){
+        return !(*this<other);
+    }
+    rational operator+(const rational &other){
+        if(deno==0) return *this;
         if(other.deno==0) return other;
         long long new_deno=deno/gcd(deno,other.deno)*other.deno;
         return rational(new_deno/deno*num + new_deno/other.deno*other.num, new_deno);
@@ -79,7 +63,7 @@ struct rational{
         other.num*=-1;
         return *this+other;
     }
-    rational operator*(rational &other){
+    rational operator*(const rational &other){
         long long g=gcd(num,other.deno);
         return rational(num/g*other.num,other.deno/g*deno);
     }
@@ -87,8 +71,8 @@ struct rational{
         swap(other.deno,other.num);
         return *this*other;
     }
-    void operator+=(rational &other){*this=*this+other;}
-    void operator-=(rational &other){*this=*this-other;}
-    void operator*=(rational &other){*this=*this*other;}
-    void operator/=(rational &other){*this=*this/other;}
+    void operator+=(const rational &other){*this=*this+other;}
+    void operator-=(const rational &other){*this=*this-other;}
+    void operator*=(const rational &other){*this=*this*other;}
+    void operator/=(const rational &other){*this=*this/other;}
 };
